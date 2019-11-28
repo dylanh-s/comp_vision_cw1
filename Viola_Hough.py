1efb306ed5fcee2b2bccec0b4ef87ff03106536d
alexjcarolan <alexjcarolan@gmail.com>
2019-11-28 15:22:51 +0000

    Viola Hough fix

--- Viola_Hough.py
+++ Viola_Hough.py
@@ -32,7 +32,7 @@ def sobel(image):
     direction_image[direction_image < -np.pi/2] += np.pi
     return magnitude_image, direction_image
 
-def hough(image_name, image_type, rmin, rmax, rinc, ainc, t1, t2, t3):
+def hough(image_name, rmin, rmax, rinc, ainc, t1, t2, t3):
     image = cv.imread(image_name)
     image_grey = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
     image_number = int(image_name[5:-4])
@@ -50,26 +50,25 @@ def hough(image_name, image_type, rmin, rmax, rinc, ainc, t1, t2, t3):
     magnitude_image, direction_image = sobel(image_grey)
     for y in range(height):
         for x in range(width):
-            if (image_type == "circles"):
-                for r in range(rmin, rmax, rinc): 
-                    if magnitude_image[y, x] > t1:
-                        yp = y+(r*np.sin(direction_image[y, x]))
-                        xp = x+(r*np.cos(direction_image[y, x]))
-                        yn = y-(r*np.sin(direction_image[y, x]))
-                        xn = x-(r*np.cos(direction_image[y, x]))
+            for r in range(rmin, rmax, rinc): 
+                if magnitude_image[y, x] > t1:
+                    yp = y+(r*np.sin(direction_image[y, x]))
+                    xp = x+(r*np.cos(direction_image[y, x]))
+                    yn = y-(r*np.sin(direction_image[y, x]))
+                    xn = x-(r*np.cos(direction_image[y, x]))
+                    
+                    if (yp > 0 and yp < height and xp > 0 and xp < width):
+                        circle_space[r, int(yp), int(xp)] += 1
+                    if (yn > 0 and yn < height and xn > 0 and xn < width):
+                        circle_space[r, int(yn), int(xn)] += 1
                         
-                        if (yp > 0 and yp < height and xp > 0 and xp < width):
-                            circle_space[r, int(yp), int(xp)] += 1
-                        if (yn > 0 and yn < height and xn > 0 and xn < width):
-                            circle_space[r, int(yn), int(xn)] += 1
-            elif (image_type == "lines"):
-                amin = int(np.clip(np.floor(np.degrees(direction_image[y, x])-6), 0, 360))
-                amax = int(np.clip(np.floor(np.degrees(direction_image[y, x])+6+1), 0, 360))
-                if ((amin > 10 and amax < 80) or (amin > 100 and amax < 170) or (amin > 190 and amax < 260) or (amin > 280 and amax < 350)):
-                    for a in range(amin, amax, ainc): 
-                        if magnitude_image[y, x] > t1:
-                            d = int(np.floor((x-width//2)*np.cos(np.radians(a))+(y-height//2)*np.sin(np.radians(a))))
-                            line_space[d, a] += 1
+            amin = int(np.clip(np.floor(np.degrees(direction_image[y, x])-6), 0, 360))
+            amax = int(np.clip(np.floor(np.degrees(direction_image[y, x])+6+1), 0, 360))
+            if ((amin > 10 and amax < 80) or (amin > 100 and amax < 170) or (amin > 190 and amax < 260) or (amin > 280 and amax < 350)):
+                for a in range(amin, amax, ainc): 
+                    if magnitude_image[y, x] > t1:
+                        d = int(np.floor((x-width//2)*np.cos(np.radians(a))+(y-height//2)*np.sin(np.radians(a))))
+                        line_space[d, a] += 1
 
     circle_space = threshold(circle_space, t2)
     circle_image = np.sum(circle_space, axis=0)
